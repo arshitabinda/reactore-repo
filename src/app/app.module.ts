844b96867ed1c7490de1e8bfa7b6398687ddaf9d
tabinda <tabinda.m@reactore.com>
2018-08-21 18:11:30 +0530

    removed hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { MatTableModule } from '@angular/material';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatTableModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatTableModule
+  ],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
